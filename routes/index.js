5e9be7bc9f971921599feb554d745568b4bcf090
zzy96 <zhouzhiyaohflser@gmail.com>
2018-02-03 12:16:37 +0800

    make practice public, ui refine

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var uc = require('../controllers/userController');
-var ac = require('../controllers/authenticationController');
 
 /* home page */
 router.get('/', uc.checkLoginStatus);
@@ -16,10 +15,10 @@ router.get('/logout', uc.logout);
 router.post('/status', uc.updateStatus);
 
 /* practice */
-router.get('/practice', ac.checkLoginStatus, uc.practice);
+router.get('/practice', uc.practice);
 
 /* get questions */
-router.get('/questions/:n', ac.checkLoginStatus, uc.questions)
+router.get('/questions/:n', uc.questions)
 
 /* play */
 router.get('/play/:username', uc.play);
